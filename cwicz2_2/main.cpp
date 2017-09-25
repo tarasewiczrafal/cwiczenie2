0020ec81eaf5825e843adcd5945b46670230a6ae
tarasewiczrafal <32269516+tarasewiczrafal@users.noreply.github.com>
2017-09-25 16:59:57 +0200

    lecimydalej

--- cwicz2_2/main.cpp
+++ cwicz2_2/main.cpp
@@ -5,11 +5,20 @@ using namespace std;
 
 int main()
 {
-    string imie = "Rafal";
-    string nazwisko = "Tarasewicz";
-    int rok_urodzenia = 1996;
-    int akualny_rok = 2017;
-    int wiek = akualny_rok - rok_urodzenia;
-    cout << "Witam, nazywam sie "<<imie << " "<< nazwisko << " "<< "urodzilem sie w" << " " <<rok_urodzenia << " " << "i mam"<< " "<< wiek << " "<< "lat" << endl;
+    string imie ;
+    string nazwisko ;
+    int rok_urodzenia;
+    int akualny_rok;
+
+    cout << "Podaj imie ";
+    cin>> imie;
+    cout << "Podaj nazwisko ";
+    cin >> nazwisko;
+    cout << "podaj rok urodzenia ";
+    cin >> rok_urodzenia;
+    cout << "podaj dzisiejszy rok ";
+    cin >>akualny_rok;
+int wiek = akualny_rok - rok_urodzenia;
+    cout << "Witam, nazywam sie "<<imie << " "<< nazwisko << " "<< "urodzilem sie w" << " " <<rok_urodzenia << " " << "roku" << " "<< "i mam"<< " "<< wiek << " "<< "lat" << endl;
     return 0;
 }
